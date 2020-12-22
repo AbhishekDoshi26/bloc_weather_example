f510ef9290b5470d36356c825be8d41cb7b6ec9a
Abhishek Doshi <adoshi26.ad@gmail.com>
2020-12-22 14:55:58 +0530

    Added BlocProvider in main.dart

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_weather_example/bloc/weather_bloc.dart';
 import 'package:bloc_weather_example/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +11,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: BlocProvider(
+        create: (context) => WeatherBloc(),
+        child: Home(),
+      ),
     );
   }
 }
