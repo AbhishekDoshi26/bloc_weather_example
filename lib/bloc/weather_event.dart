de9dce89856de70485a872e7a3df00dcb21490df
Abhishek Doshi <adoshi26.ad@gmail.com>
2020-12-22 14:48:01 +0530

    Added Events

--- lib/bloc/weather_event.dart
+++ lib/bloc/weather_event.dart
@@ -2,3 +2,14 @@ part of 'weather_bloc.dart';
 
 @immutable
 abstract class WeatherEvent {}
+
+class WeatherInitialEvent extends WeatherEvent {}
+
+class WeatherLoadingEvent extends WeatherEvent {}
+
+class WeatherGetEvent extends WeatherEvent {
+  final String city;
+  WeatherGetEvent({this.city});
+}
+
+class WeatherLoaded extends WeatherEvent {}
